67e5f28642256bca9b8bfa96180aa5beb7c6da84
eshginfarzali <eshqinferzeliyev@gmail.com>
2026-01-06 17:51:32 +0400

    Initial Commit

---
+++ LOTRConvertor/ContentView.swift
@@ -0,0 +1,109 @@
+//
+//  ContentView.swift
+//  LOTRConvertor
+//
+//  Created by OBA Market on 25.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            //Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                //Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .frame(width: 200)
+          
+                
+                // Currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                //Conversion section
+                HStack{
+                    // Left converion section
+                    VStack{
+                        // Currency
+                        HStack{
+                            // Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                                .frame(width: 33)
+                            
+                            // Currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        
+                        //Text field
+                        Text("Text field")
+                    }
+                    
+                    // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    // Right conversion section
+                    VStack{
+                        // Currency
+                        HStack{
+                            // Currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+
+                            
+                            // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                                .frame(width: 33)
+                            
+                            
+                        }
+                        
+                        //Text field
+                        Text("Text field")
+                    }
+
+                }
+                Spacer()
+                
+                //Info button
+                Button{
+                    
+                } label: {
+                                    Image(systemName: "info.circle.fill")
+                                        .font(.largeTitle)
+                                        .foregroundStyle(.white)
+                }
+                //bele de ola biler yuxarida da
+//                .font(.largeTitle)
+//                .foregroundStyle(.white)
+
+               
+            }
+//            .border(.blue)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConvertor/LOTRConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConvertorApp.swift
+//  LOTRConvertor
+//
+//  Created by OBA Market on 25.12.25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
